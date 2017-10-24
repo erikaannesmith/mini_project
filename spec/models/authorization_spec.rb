2c8bc6f9b4e0bea18b30d3f8b797880690f0e718
Erika Smith <erikaannesmith@gmail.com>
2017-10-24 16:54:19 -0600

    Complete user role tests

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -12,4 +12,32 @@ describe User do
         expect(user.role).to eq("default")
     end
 
+    it "they can be created as a default user" do
+        user = User.create(email: "abc",
+                           password: "123",
+                           company_name: "Yas",
+                           website: "www.yas.com",
+                           location: "YASland",
+                           role: 0)
+
+        expect(user).to be_valid
+        expect(user.role).to eq("default")
+    end
+
+    it "they are created as an admin user" do
+        user = User.create(email: "abc",
+                           password: "123",
+                           company_name: "Yas",
+                           website: "www.yas.com",
+                           location: "YASland",
+                           role: 1)
+
+        expect(user).to be_valid
+        expect(user.role).to eq("admin")
+
+        user.default!
+
+        expect(user.default?).to be_truthy
+    end
+
 end
